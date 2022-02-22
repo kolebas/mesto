4f820366571e2bf54fa9913b0ca948d1a8471dba
Николай <kolebas1986@gmail.com>
2022-02-22 11:13:39 +0300

    fix: изменить все согласно замечаниям ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -29,64 +29,16 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 
-function createCards() {         
-  let card = document.createElement ('div');
-  card.className = "cards";  
-  let sectionCards = document.querySelector('.page__cards');  
-  let cards = [
-    {
-      title: "Карачевск",
-      image: "./images/elbrus.jpg"
-    },
-    {
-      title: "Гора Эльбрус",
-      image: "./images/dombai.jpg"
-    },
-    {
-      title: "Домбай",
-      image: "./images/elbrus.jpg"
-    },
-    {
-      title: "Гора Эльбрус",
-      image: "./images/dombai.jpg"
-    },
-    {
-      title: "Карачаево-Черкесия",
-      image: "./images/elbrus.jpg"
-    },    
-    {
-      title: "Домбай",
-      image: "./images/dombai.jpg"
-    },
-  ];
-  cards.forEach(element => 
-    sectionCards.insertAdjacentHTML('beforeend',`
-      <div class="cards__item">
-        <img class="card__image" src="` + element['image'] + `" alt="">
-        <div class="card__rectangle">
-          <h2 class="card__title">` + element['title'] + `</h2>
-          <img class="card__like-button" src="./images/card-heart.svg" alt=""/>
-        </div>
-      </div>`),     
-  )           
-
-}
-function showFormEdit(){
-  if(formSection && formElement){
-    formSection.classList.add('page__popup_show');  
-    formElement.classList.add('popup_show');
-  }  
+function showFormEdit(){  
+  formElement.classList.add('popup_opened');  
   inputs = document.querySelectorAll('input');
   inputs[0].value = discoverName.textContent;
   inputs[1].value = discoverJob.textContent;  
 }
 
 function closeFormEdit(){
-  formSection.classList.remove('page__popup_show'); 
-  formElement.classList.remove('popup_show');
+  formSection.classList.remove('popup_opened');
 }
-
-document.body.onload = createCards;
 
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', showFormEdit);
